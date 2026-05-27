f5b3b4aedb8d5b7d80223e2e5adb6e295ac83260
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-28 00:14:21 +0800

    mongodb shell script added

---
+++ 01-mongodb.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+LOGS_DIR="/var/logs/roboshop"
+
+sudo mkdir -p $LOGS_DIR
+sudo chown -R ec2-user:ec2-user $LOGS_DIR
+sudo chmod -R 755 $LOGS_DIR
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+TIMESTAMP=$(date "+%Y-%m-%d %h:%M:%S")
+
+if [ $USERID -ne =0 ]; then
+    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Monog repo"
