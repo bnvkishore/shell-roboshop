be2af5bba520b5fe114777b6ab127a1032199c62
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-30 01:07:03 +0800

    mongodb shell script added

--- roboshop.sh
+++ roboshop.sh
@@ -51,7 +51,7 @@ do
                         "TTL": 1,
                         "ResourceRecords": [
                             {
-                                "Value": "'$IP"
+                                "Value": "'$IP'"
                             }
                         ]
                     }
