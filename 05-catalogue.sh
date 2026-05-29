93a598029b49214fd54791e3e20475be27b2edae
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-30 01:46:23 +0800

    mongodb shell script added

---
+++ 05-catalogue.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+LOGS_DIR="/var/logs/roboshop"
+
+sudo mkdir -p $LOGS_DIR
+sudo chown -R ec2-user:ec2-user $LOGS_DIR
+sudo chmod -R 755 $LOGS_DIR
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+TIMESTAMP=$(date "+%Y-%m-%d %h:%M:%S")
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>> $LOGS_FILE
+dnf module enable nodejs:20 -y &>> $LOGS_FILE
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing nodejs 20"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
+VALIDATE $? "Creating roboshop system user"
+
+mkdir /app &>> $LOGS_FILE
+VALIDATE $? "Creating app directory"
