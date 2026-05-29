942a302b0ef6990fb29a7a8c49b2e452e47d60f6
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-30 00:57:35 +0800

    mongodb shell script added

---
+++ roboshop.sh
@@ -0,0 +1,63 @@
+#!/bin/bahs
+
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z04294741VQNEFJ1F5FHE"
+DOMAIN_NAME="daws90.shop"
+
+for instance in $@
+do
+    echo "Launchin instance: $instance"
+    INSTANCE_ID=$(
+        aws ec2 run-instances \
+            --image-id "$AMI_ID" \
+            --instance-type t3.micro \
+            --security-groups "roboshop-common" "roboshop-$instance"\
+            --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+            --query 'Instances[0].InstanceId' \
+            --output text
+    )
+    echo "Instance ID: $INSTANCE_ID"
+
+    if [ $instance == "frontend"]; then
+        IP=$(
+            aws ec2 describe-instances 
+                --instance-ids $INSTANCE_ID \
+                --query "Reservations[*].Instances[*].PublicIpAddress" \
+                --output text
+            )
+        R53_RECORD="$DOMAIN_NAME"
+    else
+        IP=$(
+            aws ec2 describe-instances 
+                --instance-ids $INSTANCE_ID \
+                --query "Reservations[*].Instances[*].PrivateIpAddress" \
+                --output text
+            )
+        R53_RECORD="$instance.$DOMAIN_NAME"
+    fi
+
+    #### Update route53 record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id "$ZONE_ID" \
+    --change-batch '
+        {
+            "Comment": "Update record to new IP address",
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": "example.com",
+                        "Type": "A",
+                        "TTL": 1,
+                        "ResourceRecords": [
+                            {
+                                "Value": "192.0.2.44"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    '
+
+done
