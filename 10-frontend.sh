64e8d2f13d1a2ea2a33515c660f71779f703bac5
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-30 18:33:57 +0800

    mongodb shell script added

--- 10-frontend.sh
+++ 10-frontend.sh
@@ -46,7 +46,7 @@ VALIDATE $?  "Downloaded and extracted frontend code"
 rm -rf /etc/nginx/nginx.conf
 VALIDATE $?  "Removed Default conf"
 
-cp nginx.conf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $?  "Copied roboshop nginx conf"
 
 systemctl enable nginx 
